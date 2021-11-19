e09919f7be63fe4dc6a0fea2a45b3a3fea50fd3a
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-20 08:24:44 +0900

    リポジトリにラムダ式の追加。

--- src/main/java/com/example/repository/CongestionSituationRepository.java
+++ src/main/java/com/example/repository/CongestionSituationRepository.java
@@ -1,5 +1,27 @@
 package com.example.repository;
 
-public class CongestionSituationRepository {
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import com.example.domain.CongestionSituationDomain;
+
+/**
+ * DBアクセス用のリポジトリクラス
+ * @author manami
+ *
+ */
+@Repository
+public class CongestionSituationRepository {
+	private static final RowMapper<CongestionSituationDomain> CONGESTION_SITUATION_DOMAIN_ROW_MAPPER = (rs,i) -> {
+		CongestionSituationDomain domain = new CongestionSituationDomain();
+		domain.setId(rs.getInt("id"));
+		domain.setName(rs.getString("name"));
+		domain.setMaleComfortableNumberOfPeople(rs.getInt("male_comfortable_number_of_people"));
+		domain.setMaleALittleNumberOfPeople(rs.getInt("male_a_little_number_of_people"));
+		domain.setFemaleCongestionNumberOfPeople(rs.getInt("male_congestion_number_of_people"));
+		domain.setFemaleComfortableNumberOfPeople(rs.getInt("female_comfortable_number_of_people"));
+		domain.setFemaleALittleNumberOfPeople(rs.getInt("female_a_little_number_of_people"));
+		domain.setFemaleCongestionNumberOfPeople(rs.getInt("female_congestion_number_of_people"));
+		return domain;
+	};
 }
