5551099237e9f57afa8b7c89439f629d8b09c0ca
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-20 07:29:12 +0900

    first commit!

---
+++ src/main/java/com/example/CongestionSituationApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongestionSituationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongestionSituationApplication.class, args);
+	}
+
+}
