1e2869aa294823a6a601b8872e6983a13b1ba245
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-20 08:41:27 +0900

    リポジトリクラスに下記のメソッドを追加。 ・findAllメソッド：全件検索用のメソッド ・loadメソッド：引数のidで１レコードを取得するためのメソッド

--- src/main/java/com/example/repository/CongestionSituationRepository.java
+++ src/main/java/com/example/repository/CongestionSituationRepository.java
@@ -1,6 +1,12 @@
 package com.example.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.CongestionSituationDomain;
@@ -24,4 +30,29 @@ public class CongestionSituationRepository {
 		domain.setFemaleCongestionNumberOfPeople(rs.getInt("female_congestion_number_of_people"));
 		return domain;
 	};
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	/**
+	 * 施設一覧を取得するためのメソッド
+	 * @return 施設の全件情報を取得する
+	 */
+	public List<CongestionSituationDomain>  findAll() {
+		String sql = "SELECT * FROM sauna_list ORDER BY id";
+		List<CongestionSituationDomain> domainList = template.query(sql, CONGESTION_SITUATION_DOMAIN_ROW_MAPPER);
+		return domainList;
+	}
+	
+	/**
+	 * idで該当情報を取得するためのメソッド
+	 * @param Integer id
+	 * @return 引数のidで検索した該当のレコードを取得する
+	 */
+	public CongestionSituationDomain load(Integer id) {
+		String sql = "SELECT * FROM sauna_list WHERE id=:id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		CongestionSituationDomain domain = template.queryForObject(sql, param, CONGESTION_SITUATION_DOMAIN_ROW_MAPPER);
+		return domain;
+	}
 }
