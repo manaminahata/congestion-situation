c50628c8a4bcf08931ff73c5a8680add710e0b9a
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-20 12:53:51 +0900

    サービスクラスの作成。全件取得用showListメソッドを追加。

---
+++ src/main/java/com/example/service/CongestionSituationService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.domain.CongestionSituationDomain;
+import com.example.repository.CongestionSituationRepository;
+
+@Service
+public class CongestionSituationService {
+	@Autowired
+	private CongestionSituationRepository repository;
+	
+	/**
+	 * 施設一覧取得用のメソッド
+	 * @return　施設の全件情報を取得する
+	 */
+	public List<CongestionSituationDomain> showList() {
+		List<CongestionSituationDomain> domainList = repository.findAll();
+		return domainList;
+	}
+	
+	
+}
