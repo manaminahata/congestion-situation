57912e6b08bfe5350852439586db14edd6e8d129
NahataManami <manami.nahata@rakus-partners.co.jp>
2021-11-21 15:32:29 +0900

    コントローラーの修正。下記メソッドの追加。 ・loginメソッド：管理者ログインページの表示 ・insertメソッド：新規登録用ページの表示 ・additionCompleted：施設登録用のメソッド。施設の追加後、ログインページに戻るよう設定。

--- src/main/java/com/example/controller/CongestionSituationController.java
+++ src/main/java/com/example/controller/CongestionSituationController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,8 +36,46 @@ public class CongestionSituationController {
 		return "sauna-list";
 	}
 	
+	/**
+	 * 管理者ログイン用ページを表示させる
+	 * @return login.html
+	 */
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
 	}
+	
+	/**
+	 * 新規登録用のページを表示させる
+	 * @return insert.html
+	 */
+	@RequestMapping("/insert")
+	public String insert() {
+		return "insert";
+	}
+	
+	/**
+	 * 施設の登録
+	 * 登録完了後、ログインページに遷移させる
+	 * @param form
+	 * @param model
+	 * @return login.html
+	 */
+	@RequestMapping("/addition-completed")
+	public String additionCompleted(CongestionSituationForm form, Model model) {
+		CongestionSituationDomain domain = new CongestionSituationDomain();
+		BeanUtils.copyProperties(form, domain);
+		domain.setMaleComfortableNumberOfPeople(form.getIntMaleComfortableNumberOfPeople());
+		domain.setMaleALittleNumberOfPeople(form.getIntMaleALittleNumberOfPeople());
+		domain.setMaleCongestionNumberOfPeople(form.getIntMaleCongestionNumberOfPeople());
+		domain.setFemaleComfortableNumberOfPeople(form.getIntFemaleComfortableNumberOfPeople());
+		domain.setFemaleALittleNumberOfPeople(form.getIntFemaleALittleNumberOfPeople());
+		domain.setFemaleCongestionNumberOfPeople(form.getIntFemaleCongestionNumberOfPeople());
+		service.insert(domain);
+		return "redirect:login";
+	}
+	
+	
+	
+	
 }
